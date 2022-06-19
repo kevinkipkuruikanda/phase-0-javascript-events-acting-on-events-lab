2addd8401fa5950203e57926a344e7a832719c3e
kevin@kevin <kevin1kipkurui@gmail.com>
2022-06-19 12:34:46 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Your code here
+const input = document.querySelector("input");
+
+function addingEventListener() {
+  input.addEventListener("click", function (event) {
+    return "clicked";
+  });
+}
